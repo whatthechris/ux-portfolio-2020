260235a84f2c041495aa355e16353210843b8d72
Chris Wong <chris@Chriss-MacBook-Pro.local>
2019-11-16 19:55:32 -0500

    Added small project cards

--- src/components/project_card.js
+++ src/components/project_card.js
@@ -3,30 +3,30 @@ import { Link } from "gatsby"
 
 import "./styles/project_card.scss"
 
-const ProjectCard = () => (
-    <div className="project-card">
+const ProjectCard = props => (
+    <div className={"project-card" + (props.smallcard ? ' project-card--small' : '')}>
         <Link to="/page-2/">
-        <div className="project-card__content">
-            <div className="project-card__text">
+        <div className={"project-card__content" + (props.smallcard ? ' project-card__content--small' : '')}>
+            <div className={"project-card__text" + (props.smallcard ? ' project-card__text--small' : '')}>
                 <div className="project-card__top-text">
-                    <p className="project-card__eyebrow">Wayfinder Web App</p>
-                    <h2>Helping hard working people save for the good life</h2>
+                    <p className="project-card__eyebrow">{props.eyebrow}</p>
+                    <h2>{props.heading}</h2>
                 </div>
                 <div className="project-card__bottom-text">
-                    <ul className="project-card__roles">
+                    <ul className={"project-card__roles" + (props.smallcard ? ' project-card__roles--small' : '')}>
                         <h5>Roles</h5>
                         <p>Motion Design</p>
                         <p>UI/ UX Design</p>
                     </ul>
-                    <a className="text-link">View Case Study</a>
+                    <a className="text-link">{props.textlink}</a>
                     <img src={require('../images/svgs/arrow-right.svg')} />
                 </div>
             </div>
-            <div className="project-card__image">
-                <img src={require('../images/homepage/wayfinder_thumb.png')} />
+            <div className={"project-card__image" + (props.smallcard ? ' project-card__image--small' : '')}>
+                <img src={props.thumbnail} className={(props.smallcard ? ' project-card__image--smallimg' : '')}/>
             </div>
         </div>
-        <div className="project-card__highlight"></div>
+        <div className={"project-card__highlight" + (props.smallcard ? ' project-card__highlight--small' : '') + (props.sand ? ' project-card__highlight--sand' : '') + (props.moss ? ' project-card__highlight--moss' : '') + (props.earth ? ' project-card__highlight--earth' : '')}></div>
         </Link>
     </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import "../data/page_styles.scss"
+import "./styles/homepage.scss"
 import HomepageHero from "../components/homepage_hero.js"
 import PullQuote from "../components/pull_quote.js"
 import ProjectCard from "../components/project_card.js"
@@ -9,9 +10,41 @@ import ProjectCard from "../components/project_card.js"
 const IndexPage = () => (
     <div>
       <HomepageHero />
-      <div className="page--container">
+      <div className="page-container">
         <PullQuote />
-        <ProjectCard />
+        <ProjectCard 
+          eyebrow="Wayfinder Web App"
+          heading="Helping hard working people save for the good life"
+          textlink="View case study"
+          thumbnail={require('../images/homepage/wayfinder_thumb.png')}
+          sand="true"
+        />
+        <div className="small-cards">
+          <ProjectCard 
+            smallcard="true"
+            eyebrow="PC Optimum Web App"
+            heading="Levelling up Loblaw's loyalty points game"
+            textlink="View case study"
+            thumbnail={require('../images/homepage/pcoptimum_thumb.png')}
+            sand="true"
+          />
+          <ProjectCard 
+            smallcard="true"
+            eyebrow="The Canadian Innovation Awards"
+            heading="Celebrating Canadian tech innovation"
+            textlink="View case study"
+            thumbnail={require('../images/homepage/cia_thumb.png')}
+            moss="true"
+          />
+          <ProjectCard 
+            smallcard="true"
+            eyebrow="Various Motion Design Projects"
+            heading="Communicating complex ideas through motion"
+            textlink="View projects"
+            thumbnail={require('../images/homepage/wayfinder_thumb.png')}
+            earth="true"
+          />
+        </div>
       </div>
     </div>
 )
