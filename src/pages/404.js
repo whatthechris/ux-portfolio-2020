5476fa4536638389c62a19a397a93ae19a817776
Chris Wong <chris@Chriss-MacBook-Pro.local>
2020-02-25 14:20:42 -0500

    Added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,20 @@ import React from "react"
 import { Title, Meta } from 'react-head';
 import Menu from "../components/menu.js"
 
+import "./styles/404.scss"
+
 const NotFoundPage = () => (
   <div>
     <Title>404</Title>
     <Meta name="robots" content="noindex, nofollow" />
     <Menu/>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="missing">
+      <div className="missing__text">
+        <span>¯\_(ツ)_/¯</span>
+        <h2>404 Page not found</h2>
+        <p>Oh hi, seems like the page you're looking for doesn't exist. Click the menu button and let's get you back on track.</p>
+      </div>
+    </div>
   </div>
 )
 
