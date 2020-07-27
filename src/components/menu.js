4b2f9f29d230294065b169bcb777d111728e046a
whatthechris <cjordanwong@gmail.com>
2020-07-27 17:58:14 -0400

    Added icons and mobile styles to fab

--- src/components/menu.js
+++ src/components/menu.js
@@ -20,19 +20,21 @@ class Menu extends React.Component {
             <div>
                 <div className="menu__text">
                     <h3>Hey there!</h3>
-                    <p>I’m Chris, a product designer from Toronto currently looking for remote and  over seas opportunities.<br/><br/>
+                    <p className="caption">I’m Chris, a product designer from Toronto currently looking for remote and  over seas opportunities.<br/><br/>
                     I’m a designer who codes, an animator who illustrates and a life-long learner who teaches. I use this unique combination of skills to create exciting and thoughtful digital experiences focusing on the needs of everyday people.<br/><br/>
                     On the side, I create <a href="" target="_blank" rel="noopener noreferrer">motion graphics</a> that move people and make <a href="https://gifmusic2.netlify.app" target="_blank" rel="noopener noreferrer">fun web projects</a> that empower others to create.
                     </p>
-                    <br/><br/>
-                    <h5>Let's grab a coffee</h5>
-                    <p>hello@cwdigital.ca</p>
+                    <div className="menu__cta">
+                        <img src={require(`../images/svgs/wave-white.svg`)} alt="Hand wave icon"/>  
+                        <h5>Let's grab a coffee</h5>
+                        <p className="caption">hello@cwdigital.ca</p>
+                    </div>
                 </div>
             <img className="menu__icon" src={require(`../images/svgs/close.svg`)} alt="Close icon"/>
             </div>
             }
             {!this.state.isMenuOpen &&
-                <img className="menu__icon" src={require(`../images/svgs/wave.svg`)} alt="Menu icon"/>  
+                <img className="menu__icon" src={require(`../images/svgs/wave.svg`)} alt="Hand wave icon"/>  
             }
             <div className={this.state.isMenuOpen ? "menu__circle-open" : "menu__circle"}></div>
         </button>
