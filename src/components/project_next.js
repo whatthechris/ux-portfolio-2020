75f79c029b989b76a6f668dfc127c59e099646e0
whatthechris <cjordanwong@gmail.com>
2020-08-02 13:55:53 -0400

    Fixed carousel mq styles

--- src/components/project_next.js
+++ src/components/project_next.js
@@ -6,17 +6,17 @@ import "./styles/project_next.scss"
 const ProjectNext = props => (
   <a href={props.link}>
     <div className={(`project-next ${props.bgcolor}`)}>
-      <Fade bottom distance="100px">
         <div className="project-next__container">
-          <div className="project-next__image-container">
-            <img src={require(`../images/${props.image}`)} alt={props.imagealt}/>
-          </div>
-          <div className="project-next__text">
-            <h3>{props.heading}</h3>
-            <p>{props.subheading}</p>
-          </div>
+          <Fade bottom distance="100px">
+            <div className="project-next__image-container">
+              <img src={require(`../images/${props.image}`)} alt={props.imagealt}/>
+            </div>
+            <div className="project-next__text">
+              <h3>{props.heading}</h3>
+              <p>{props.subheading}</p>
+            </div>
+          </Fade>
         </div>
-      </Fade>
     </div>
   </a>
 )
