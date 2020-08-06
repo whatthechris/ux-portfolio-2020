3310b8e0bc6aee56b6829cc7c4e00b829e790a18
whatthechris <cjordanwong@gmail.com>
2020-08-06 10:29:01 -0400

    Updated 404 and button styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Title, Meta } from 'react-head';
 import Menu from "../components/menu.js"
+import Button from "../components/button.js"
 
 import "./styles/404.scss"
 
@@ -10,11 +11,15 @@ const NotFoundPage = () => (
     <Meta name="robots" content="noindex, nofollow" />
     <Menu/>
     <div className="missing">
+      <span>¯\_(ツ)_/¯</span>
       <div className="missing__text">
-        <span>¯\_(ツ)_/¯</span>
         <h2>404 Page not found</h2>
-        <p>Oh hi, seems like the page you're looking for doesn't exist. Click the menu button and let's get you back on track.</p>
+        <p>Oh hi, seems like the page you're looking for doesn't exist. Click the button below and let's get you back on track.</p>
       </div>
+      <Button
+          text="Back to Homepage"
+          link="/"
+        />
     </div>
   </div>
 )
